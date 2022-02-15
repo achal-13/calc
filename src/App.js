924ffc57c731e1435b045a967e21c50d952f7c4e
AchalSharma3 <achal.sharma@str8bat.com>
2022-02-15 23:31:11 +0530

    Add calculator html structure with css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  function createNumbers () {
+    const numbers = [];
+
+    for(let i=9; i>=0; i--) {
+      numbers.push(
+        <button key={i}>{i}</button>
+      )
+    }
+    return numbers
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='wrapper'>
+        <div className='display'>
+          123
+        </div>
+        <div className='inputs'>
+          <div className='numbers'>
+            {createNumbers()}
+            <button>.</button>
+            <button>=</button>
+          </div>
+          <div className='operations'>
+            <button>+</button>
+            <button>-</button>
+            <button>/</button>
+            <button>*</button>
+            <button>%</button>
+            <button>^</button>
+            <button>(</button>
+            <button>)</button>
+            <button>DEL</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
